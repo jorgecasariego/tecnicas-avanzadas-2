ec379f47770ca9504d12d10368d911a66789165e
Jorge Casariego <jorgekasa@gmail.com>
2019-11-12 17:26:58 -0300

    Inicio clase 7

---
+++ Clase7/app/src/main/java/py/edu/uca/clase7/MainActivity.kt
@@ -0,0 +1,12 @@
+package py.edu.uca.clase7
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
