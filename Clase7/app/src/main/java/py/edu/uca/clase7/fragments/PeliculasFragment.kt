b1f1dcd43d375d1b9603c764acfab9efe71a01b7
Jorge Casariego <jorgekasa@gmail.com>
2019-12-03 14:52:53 -0300

    Se inicializan las variables en el fragment de peliculas

--- Clase7/app/src/main/java/py/edu/uca/clase7/fragments/PeliculasFragment.kt
+++ Clase7/app/src/main/java/py/edu/uca/clase7/fragments/PeliculasFragment.kt
@@ -6,16 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import py.edu.uca.clase7.R
+import py.edu.uca.clase7.adapter.PeliculasAdapter
+import py.edu.uca.clase7.model.Pelicula
 
 class PeliculasFragment : Fragment() {
+
+    private lateinit var listaPeliculasRv: RecyclerView
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: PeliculasAdapter
+    private val peliculas: MutableList<Pelicula> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_peliculas, container, false)
+        val view =  inflater.inflate(R.layout.fragment_peliculas, container, false)
+
+        listaPeliculasRv = view.findViewById(R.id.lista_peliculas)
+
+        linearLayoutManager = LinearLayoutManager(requireContext())
+        listaPeliculasRv.layoutManager = linearLayoutManager
+
+        adapter = PeliculasAdapter(requireContext(), peliculas as ArrayList<Pelicula>)
+        listaPeliculasRv.adapter = adapter
+
+        return view
     }
 
 
